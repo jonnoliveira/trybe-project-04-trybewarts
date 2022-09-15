902d7522d2a424023ce2c7f9a1d7208c3e0e27bd
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-09-14 21:53:08 -0300

    Requisito 20 completo

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 // SELETORES
-const btnEnviar = document.getElementById('btn-enviar');
+const btnSend = document.getElementById('btn-enviar');
 const inputEmail = document.getElementById('email');
-const inputSenha = document.getElementById('password');
-const validEnviar = document.getElementById('submit-btn');
+const inputPassword = document.getElementById('password');
+const validSend = document.getElementById('submit-btn');
 const agreeCheck = document.getElementById('agreement');
+const textArea = document.getElementById('textarea');
+const resultCount = document.getElementById('counter');
 
 // VERIFICAÇÃO BOTÃO ENTRAR FORM
 
-function validacaoEmailSenha() {
-  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+function validEmailPassword() {
+  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-  console.log(inputEmail.value, inputSenha.value);
 }
 
-btnEnviar.addEventListener('click', validacaoEmailSenha);
+btnSend.addEventListener('click', validEmailPassword);
 
-function validarCheckbox() {
+// VERIFICAÇÃO BOTÃO ENVIAR FORM
+
+function validCheckbox() {
   if (agreeCheck.checked === false) {
-    validEnviar.disabled = true;
+    validSend.disabled = true;
   } else {
-    validEnviar.disabled = false;
+    validSend.disabled = false;
   }
 }
 
-agreeCheck.addEventListener('click', validarCheckbox);
+agreeCheck.addEventListener('click', validCheckbox);
+
+// VERIFICAÇÃO CONTADOR CARACTERES
+
+function counterCaract() {
+  const counter = textArea.value.length;
+  const maxCount = textArea.maxLength;
+  const count = maxCount - counter;
+
+  if (counter < maxCount) {
+    resultCount.innerText = `${count} ${'caracteres restantes.'}`;
+  } else if (counter === maxCount) {
+    resultCount.innerText = 'Limite de caracteres atingido!';
+  }
+  console.log(counter);
+}
+textArea.addEventListener('keyup', counterCaract);
